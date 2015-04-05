219b05a44de8cd1f81bc1f7a49f971c7c56639c1
mcheung63 <mcheung63@f11739ce-b6b4-40c4-841e-b4659f912041>
2015-04-05 04:33:23 +0000

    show form name in english

--- src/main/java/com/peterdwarf/dwarf/DebugInfoAbbrevEntry.java
+++ src/main/java/com/peterdwarf/dwarf/DebugInfoAbbrevEntry.java
@@ -5,18 +5,19 @@ import com.peterswing.CommonLib;
 public class DebugInfoAbbrevEntry {
 	public String name;
 	public int form;
+	public String formStr;
 	public Object value;
 	public int position;
 
 	public String toString() {
 		if (name.equals("DW_AT_low_pc") || name.equals("DW_AT_high_pc")) {
 			if (value instanceof String) {
-				return "0x" + Integer.toHexString(position) + ", " + name + ", form=" + form + ", value=0x" + CommonLib.string2long("0x" + value);
+				return "0x" + Integer.toHexString(position) + ", " + name + ", form=" + form + " (" + formStr + "), value=0x" + CommonLib.string2long("0x" + value);
 			} else {
-				return "0x" + Integer.toHexString(position) + ", " + name + ", form=" + form + ", value=0x" + Long.toHexString((Long) value);
+				return "0x" + Integer.toHexString(position) + ", " + name + ", form=" + form + " (" + formStr + "), value=0x" + Long.toHexString((Long) value);
 			}
 		} else {
-			return "0x" + Integer.toHexString(position) + ", " + name + ", form=" + form + ", value=" + value;
+			return "0x" + Integer.toHexString(position) + ", " + name + ", form=" + form + " (" + formStr + "), value=" + value;
 		}
 	}
 }
