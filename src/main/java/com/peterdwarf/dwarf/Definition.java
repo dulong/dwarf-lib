070e5eecf2f185bd3ee9ddcf26eac56111ee480e
mcheung63 <mcheung63@f11739ce-b6b4-40c4-841e-b4659f912041>
2015-05-11 20:13:34 +0000

    first two instruction from .eh_frame has been decoded.
      DW_CFA_def_cfa: r4 esp ofs 4
      DW_CFA_offset: r(8) eip at cfa -4

--- src/main/java/com/peterdwarf/dwarf/Definition.java
+++ src/main/java/com/peterdwarf/dwarf/Definition.java
@@ -1789,6 +1789,8 @@ public class Definition {
 	public static final int DW_CFA_GNU_args_size = 0x2e;
 	public static final int DW_CFA_GNU_negative_offset_extended = 0x2f;
 
+	public static final int DW_CFA_unreferenced = -1;
+
 	public static String dwarf_regnames_x86_64[] = { "rax", "rdx", "rcx", "rbx", "rsi", "rdi", "rbp", "rsp", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15", "rip", "xmm0",
 			"xmm1", "xmm2", "xmm3", "xmm4", "xmm5", "xmm6", "xmm7", "xmm8", "xmm9", "xmm10", "xmm11", "xmm12", "xmm13", "xmm14", "xmm15", "st0", "st1", "st2", "st3", "st4", "st5",
 			"st6", "st7", "mm0", "mm1", "mm2", "mm3", "mm4", "mm5", "mm6", "mm7", "rflags", "es", "cs", "ss", "ds", "fs", "gs", null, null, "fs.base", "gs.base", null, null, "tr",
