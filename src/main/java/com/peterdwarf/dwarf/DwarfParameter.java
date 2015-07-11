55407f2ff510831d13eca6185fb0f675d51270a8
mcheung63 <mcheung63@f11739ce-b6b4-40c4-841e-b4659f912041>
2015-07-11 18:46:25 +0000

    getParameterSize

--- src/main/java/com/peterdwarf/dwarf/DwarfParameter.java
+++ src/main/java/com/peterdwarf/dwarf/DwarfParameter.java
@@ -4,18 +4,20 @@ public class DwarfParameter {
 	public String name;
 	public String registerName;
 	public String type;
+	public int size;
 	public long offset;
 
-	public DwarfParameter(String name, String registerName, String type, long offset) {
+	public DwarfParameter(String name, String registerName, String type, int size, long offset) {
 		super();
 		this.name = name;
 		this.registerName = registerName;
 		this.type = type;
+		this.size = size;
 		this.offset = offset;
 	}
 
 	public String toString() {
-		return "name= " + name + ", registerName=" + registerName + ", type=" + type + ", offset=" + offset;
+		return "name= " + name + ", registerName=" + registerName + ", type=" + type + ", size=" + size + ", offset=" + offset;
 	}
 
 }
